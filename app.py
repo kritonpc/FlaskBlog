a80d4f267d6166a25ddb6814cca339c1698d9ed6
kritonpc <paranoidhax@gmail.com>
2021-12-24 01:29:57 +0200

    init

---
+++ app.py
@@ -0,0 +1,222 @@
+from flask import Flask,request, jsonify, send_file
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from link_preview import link_preview
+import uuid, json
+  
+
+from models import *
+from config import Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db, render_as_batch=True)
+app.config['SECRET_KEY'] = 'TsOpAnOxWrIaThS'
+
+cors = CORS(app, resources={r'/*': {'origins': '*'}})
+
+# from flask_socketio import SocketIO, emit, join_room, rooms
+
+
+def serializer(object):
+    ser = []
+    for item in object:
+        ser.append(item.serialize)
+    return ser
+
+# socketio = SocketIO(app, cors_allowed_origins='*')
+
+
+def VerifyUser(token):
+    if token != 'undefined':
+        token = json.loads(token)
+        user = db.session.query(User).filter_by(id=token['id']).first()
+        if user.auth_token == token['auth_token']:
+            return user
+        else:
+            return False
+    else:
+        return False
+
+
+@app.route('/api/login', methods=['POST'])
+def loginOLD():
+    data = request.json
+    # print(f'Login {data}')
+    # print(f'------------------------------------{request.remote_addr}')
+    loggedInUser = db.session.query(User).filter_by(username=data['username'].lower()).first()
+    if loggedInUser:
+        if loggedInUser.password_hash == data['password']:
+            loggedInUser.connected = True
+            loggedInUser.auth_token = uuid.uuid4().hex
+            loggedInUser.ip = request.remote_addr
+            db.session.commit()
+            auth_token = jsonify({'username':loggedInUser.username, 'id':loggedInUser.id, 'auth_token':loggedInUser.auth_token})
+            return auth_token
+    else:
+        return 'failed'
+
+
+@app.route('/api/categories', methods=['GET'])
+def getCategories():
+    categories = db.session.query(Category).all()
+    return jsonify(serializer(categories))
+
+@app.route('/api/<category>/posts', methods=['GET'])
+def getPosts(category):
+    posts = db.session.query(Post).filter_by(category_id=category).all()
+    return jsonify(serializer(posts))
+
+@app.route('/api/posts/<post_id>', methods=['GET'])
+def getPost(post_id):
+    post = db.session.query(Post).filter_by(id=post_id).first()
+    return jsonify(post.serialize)
+
+@app.route('/api/posts/create', methods=['POST'])
+def createPost():
+    data = request.json
+    # print(f'Create post {data}')
+    user = VerifyUser(data['auth_token'])
+    if user:
+        post = Post(title=data['title'], body=data['body'], category_id=data['category_id'], poster_id=['poster_id'])
+        db.session.add(post)
+        db.session.commit()
+        return 'success'
+    else:
+        return 'failed'
+
+@app.route('/api/like/<post_id>', methods=['POST'])
+def likePost(post_id):
+    data = request.json
+    user = VerifyUser(data['auth_token'])
+    if user:
+        post = db.session.query(Post).filter_by(id=post_id).first()
+        if post:
+            if user.id not in post.likes:
+                post.likes.append(user.id)
+                db.session.commit()
+                return 'success'
+            else:
+                return 'failed'
+        else:
+            return 'failed'
+    else:
+        return 'failed'
+
+@app.route('/api/dislike/<post_id>', methods=['POST'])
+def dislikePost(post_id):
+    data = request.json
+    user = VerifyUser(data['auth_token'])
+    if user:
+        post = db.session.query(Post).filter_by(id=post_id).first()
+        if post:
+            if user.id in post.likes:
+                post.likes.remove(user.id)
+                db.session.commit()
+                return 'success'
+            else:
+                return 'failed'
+        else:
+            return 'failed'
+    else:
+        return 'failed'
+
+@app.route('/api/comment/<post_id>', methods=['POST'])
+def commentPost(post_id):
+    data = request.json
+    user = VerifyUser(data['auth_token'])
+    if user:
+        post = db.session.query(Post).filter_by(id=post_id).first()
+        if post:
+            comment = Comment(body=data['body'], poster_id=user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+            return 'success'
+        else:
+            return 'failed'
+    else:
+        return 'failed'
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    data = request.json
+    user = db.session.query(User).filter_by(username=data['username'].lower()).first()
+    if user:
+        return 'failed'
+    else:
+        newUser = User(username=data['username'].lower(), password_hash=data['password'], ip=request.remote_addr, email=data['email'], description=data['description'],gender=data['gender'], dob=data['dob'], nickname=data['nickname'])
+        db.session.add(newUser)
+        db.session.commit()
+        return 'success'
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.json
+    user = db.session.query(User).filter_by(username=data['username'].lower()).first()
+    if user:
+        if user.password_hash == data['password']:
+            # user.connected = True
+            user.auth_token = uuid.uuid4().hex
+            user.ip = request.remote_addr
+            db.session.commit()
+            auth_token = jsonify({'username':user.username, 'id':user.id, 'auth_token':user.auth_token})
+            return auth_token
+        else:
+            return 'failed'
+    
+
+
+@app.route('/api/link-preview', methods=['POST','GET'])
+def linkPreview():
+    data = request.json
+    try:
+        prev = link_preview.generate_dict(data['url'])
+        return prev
+    except:
+        print('Link Not Available')
+        return {}
+    
+
+@app.route('/storage/images/<image_name>', methods=['GET'])
+def getImage(image_name):
+    # open the file with the image name from images folder
+    image_file = open(f'images/{image_name}', 'rb')
+    return send_file(image_file, mimetype='image')
+
+
+@app.route('/storage/images/<image_folder>/<image_name>', methods=['GET'])
+def getImageInFolder(image_folder,image_name):
+    # open the file with the image name from images folder
+    image_file = open(f'images/{image_folder}/{image_name}', 'rb')
+    return send_file(image_file, mimetype='image')
+
+
+# upload file route
+@app.route('/api/upload', methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+            return 'No file part'
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            print('No selected file')
+            return 'No selected file'
+        if file:
+            extension = file.filename.split('.')[-1]
+            filename = str(uuid.uuid4()) + '.' + extension
+            file.save(f'images/{filename}')
+            return filename
+        else:
+            return 'failed'
+
+if __name__ == '__main__':
+    app.debug = True
+    # socketio.run(app, host='0.0.0.0', port=2310)
+    app.run(host='0.0.0.0', port=2310)
---
+++ models.py
@@ -0,0 +1,115 @@
+from app import db
+from datetime import datetime, date
+import uuid
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+class User(db.Model):
+    id = db.Column(db.String(64), primary_key=True, default=generate_uuid)
+    gender = db.Column(db.Boolean, nullable=False)
+    dob = db.Column(db.Date, nullable=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    description = db.Column(db.String(256))
+    avatar = db.Column(db.String(256), default='avatar.png')
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    ip = db.Column(db.String(15))
+    auth_token = db.Column(db.String(32), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "gender": self.gender,
+            "dob": self.dob,
+            "username": self.username,
+            "nickname": self.nickname,
+            "description": self.description,
+            "avatar": self.avatar,
+            "email": self.email,
+            "password_hash": self.password_hash,
+            "ip": self.ip,
+            "auth_token": self.auth_token
+        }
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30))
+    desctiption = db.Column(db.String(256))
+    image = db.Column(db.String(256))
+
+    def __repr__(self):
+        return '<Category {}>'.format(self.title)
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "desctiption": self.desctiption,
+            "image": self.image
+        }
+
+
+class Post(db.Model):
+    id = db.Column(db.String(64), primary_key=True, default=generate_uuid)
+    title = db.Column(db.String(64))
+    body = db.Column(db.String(256))
+    poster_id = db.Column(db.String(64), db.ForeignKey('user.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    likes = db.relationship('Like', backref='post', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.title)
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body,
+            "poster_id": self.poster_id,
+        }
+
+class Like(db.Model):
+    id = db.Column(db.String(64), primary_key=True, default=generate_uuid)
+    post_id = db.Column(db.String(64), db.ForeignKey('post.id'))
+    user_id = db.Column(db.String(64), db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Like {}>'.format(self.id)
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "post_id": self.post_id,
+            "user_id": self.user_id,
+        }
+
+class Comment(db.Model):
+    id = db.Column(db.String(64), primary_key=True, default=generate_uuid)
+    post_id = db.Column(db.String(64), db.ForeignKey('post.id'))
+    user_id = db.Column(db.String(64), db.ForeignKey('user.id'))
+    body = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Comment {}>'.format(self.id)
+
+    @property
+    def serialize(self):
+        return {
+            "id": self.id,
+            "post_id": self.post_id,
+            "user_id": self.user_id,
+            "body": self.body,
+            "timestamp": self.timestamp,
+        }
