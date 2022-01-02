c0147a627d36dce766037569e3b5a442c8b469b1
kritonpc <paranoidhax@gmail.com>
2022-01-02 21:29:05 +0200

    Improve page info

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -6,6 +6,8 @@ import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
+document.title = 'Saidit'
+
 new Vue({
   router,
   store,
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -9,11 +9,13 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: { title: 'Saidit' }
   },
   {
     path: '/profile',
     name: 'Profile',
+    meta: { title: 'Saidit - Profile' },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -22,18 +24,21 @@ const routes = [
   {
     path: "/categories/:category",
     component: Category,
-    props: true
+    props: true,
+    meta: { title: 'Saidit' }
   },
   // login and register routes
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/Login.vue')
+    component: () => import('../views/Login.vue'),
+    meta: { title: 'Saidit' }
   },
   {
     path: '/register',
     name: 'Register',
-    component: () => import('../views/Register.vue')
+    component: () => import('../views/Register.vue'),
+    meta: { title: 'Saidit' }
   }
 ]
 
